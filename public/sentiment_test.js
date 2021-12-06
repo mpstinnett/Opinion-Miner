adb08fad6c8ff8d48d4bfa8f5c271e81ab876d5b
mpstinnett <mpstinnett@gmail.com>
2021-12-05 20:21:56 -0600

    deleted old html pages.

--- public/sentiment_test.js
+++ public/sentiment_test.js
@@ -1,27 +1,5 @@
-async function quickstart() {
-  // Imports the Google Cloud client library
-  const language = require('@google-cloud/language');
 
-  // Instantiates a client
-  const client = new language.LanguageServiceClient();
-
-  // The text to analyze
-  const text = 'Hello, world!';
-
-  const document = {
-    content: text,
-    type: 'PLAIN_TEXT',
-  };
-
-  // Detects the sentiment of the text
-  const [result] = await client.analyzeSentiment({document: document});
-  const sentiment = result.documentSentiment;
-
-  console.log(`Text: ${text}`);
-  console.log(`Sentiment score: ${sentiment.score}`);
-  console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-}
-
+const text 
 async function sentiment() {
   // Imports the Google Cloud client library
   const language = require('@google-cloud/language');
@@ -32,7 +10,7 @@ async function sentiment() {
   /**
    * TODO(developer): Uncomment the following line to run this code.
    */
-  const text = 'Im not doing well.';
+  const text = document.querySelector('[name="text]').value;
 
   // Prepares a document, representing the provided text
   const document = {
@@ -54,7 +32,6 @@ async function sentiment() {
     console.log(`  Score: ${sentence.sentiment.score}`);
     console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
   });
-}
 
-sentiment()
-//quickstart()
+  return sentence.sentiment.score;
+}
